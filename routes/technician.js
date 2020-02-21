70a5f249f8698363d656a49cb9d5071f2eb42e05
Gennaro Napolitano <gennaro@nau.edu>
2020-02-20 19:05:01 -0700

    updated templates

--- routes/technician.js
+++ routes/technician.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('pages/technic/home.ejs', { title: 'CEUSS - Technician Home' });
 });
-
+router.get('/upload_status', function(req, res, next) {
+  res.render('pages/technic/upload_status.ejs', { title: 'CEUSS - Technician Upload Status' });
+});
+router.get('/wifi_config', function(req, res, next) {
+  res.render('pages/technic/wifi_config.ejs', { title: 'CEUSS - Technician WiFi Config' });
+});
+router.get('/wifi_sim', function(req, res, next) {
+  res.render('pages/technic/wifi_sim.ejs', { title: 'CEUSS - Technician WiFi Simulation' });
+});
 module.exports = router;
