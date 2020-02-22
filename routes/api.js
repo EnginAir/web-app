fc2244c4a5681ef76d4e8458198ef2bf60833799
Gennaro Napolitano <gennaro@nau.edu>
2020-02-21 20:35:28 -0700

    correlatedFlight addon

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Wifi = require('../lib/models/wifi');
+const CorrelatedFlight = require('../lib/models/correlatedFlight');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,6 +16,21 @@ router.get('/wifi', function(req, res, next) {
 
     } else {
         Wifi.paginate({}, {page: req.query.page ? req.query.page : 1, limit: 20}, function (err, doc) {
+            if(err) {
+                throw err;
+            }
+            res.json(doc.docs);
+        });
+    }
+});
+
+router.get('/correlatedFlight', function(req, res, next) {
+    if(req.query.tailNumber) {
+
+    } else if(req.query.outcome) {
+
+    } else {
+        CorrelatedFlight.paginate({}, {page: req.query.page ? req.query.page : 1, limit: 20}, function (err, doc) {
             if(err) {
                 throw err;
             }
